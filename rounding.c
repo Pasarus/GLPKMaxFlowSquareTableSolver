3c2b717333a49382be39171813556e6b590fd007
Samuel Jones <samjones714@gmail.com>
2019-11-27 12:52:26 +0000

    Drafy of rounding.c with incorrect output

--- rounding.c
+++ rounding.c
@@ -23,42 +23,36 @@ void checkSolution(void); /* checks if a solution is valid */
 double myFloor(double x); /* slightly 'hacky' myFloor function */
 double myCeil(double x); /* slightly 'hacky' myCeil function */
 void printMatrix(char *title, double *matrix); /* print a matrix */
-void fillColumns(double*, double*,int);
-void fillRowSumValues(double *, int);
-void fillColumnSumValues(double *, int);
+void fillRValues(double *, int);
+void fillCValues(double *, int);
 
-void fillRowSumValues(double* columns, int numColumns){
+void fillRValues(double* rValues, int numR){
   int lastJPos = 0;
   int newSize = size;
-  for (int i =0; i<numColumns/2; ++i){
+  for (int i = 0; i< numR; ++i){
     double totalOfRows = 0;
     for (int j = lastJPos; j<newSize; ++j){
       totalOfRows += input[j];
       lastJPos = j+1;
     }
     newSize = newSize + lastJPos;
-    columns[i] = totalOfRows;
+    rValues[i] = totalOfRows;
   }
 }
 
-void fillColumnSumValues(double *columns, int numColumns){
+void fillCValues(double *cValues, int numC){
   int lastJPos = 0;
   int newSize = size;
   for (int i = 0; i<size; ++i){
     double totalOfRows = 0;
-    for (int j = 0; j<size; ++j){
+    for (int j = 0; j<numC; ++j){
       int index = j*size+i;
       totalOfRows += input[index];
       lastJPos = j+1;
     }
     newSize = newSize + lastJPos;
-    columns[i] = totalOfRows;
+    cValues[i] = totalOfRows;
   }
-}
-
-void fillColumns(double *columns, double *rows,int numColumns){
-  fillRowSumValues(columns, numColumns); // Fill R_n into columns
-  fillColumnSumValues(rows, numColumns); // Fill C_m into columns
 }
 
 void fillIndexArray(int* index) {
@@ -71,26 +65,35 @@ void fillIndexArray(int* index) {
 /* It is essentially empty and not functional. */
 /* Your own implementation needs to go in here. */
 int computeSolution(void) {
-  int numRows = size*size;
-  int numColumns = size + size;
-  double columns[size];
-  double rows[size];
+  int i, j; // For counting in for loops
+  const int numX = size * size; // Number of X_nm variables
+  const int numC = size; // Number of C_m variables
+  const int numR = size; // Number of R_n variables
+  const int numE = size * size; // Number of e_t variables
+  const int numVariables = numC + numR + 2*numX + numE; // this is the number of columns in the matrix for GLPK. 2*numX because of Xnm' and Xnm'' values.
+  double cPrimeValues[numC];
+  double rPrimeValues[numR];
   glp_prob *lp;
-  int index[size + 1];
-  double row[size + 1];
+  int index[numVariables + 1];
+  memset(index, 0, sizeof(index));
+  double row[numVariables + 1];
+  memset(row, 0.0, sizeof(row));
 
   lp = glp_create_prob();
   glp_set_obj_dir(lp, GLP_MAX);
 
-  glp_add_cols(lp, numColumns);
+  glp_add_cols(lp, numVariables);
 
-  fillColumns(columns, rows,numColumns);
+  fillRValues(rPrimeValues, numR);
+  fillCValues(cPrimeValues, numC);
 
-/* ADDDD ROWS AND COLUMNS BOUNDS STARTS HERE *********************************************/
-  // Set the columns upper and lower bounds
-  for (int i=1; i<=size; i++ ) {
-    double lowerBound = myFloor(columns[i-1]);
-    double upperBound = myCeil(columns[i-1]);
+/* ADD COLUMN BOUNDS STARTS HERE *********************************************/
+  // Add C_m' column variables
+  int maxLoopSize = numC;
+  for (i=1; i<=maxLoopSize; i++ ) {
+    //*2 as C_m' max and min flow is 2 times greater than C_m because the outgoing of C_m is greater by 2
+    const double lowerBound = 2*myFloor(cPrimeValues[i-1]);
+    const double upperBound = 2*myCeil(cPrimeValues[i-1]);
     if (lowerBound != upperBound) {
       glp_set_col_bnds(lp, i, GLP_DB, lowerBound, upperBound);
     } else {
@@ -98,36 +101,159 @@ int computeSolution(void) {
     }
   }
 
-  // C_m = sum([x_{im}]) and R_n = sum([x_{ni}]). All coefficients are 1.
-  for (int i=1; i<= numColumns; i++) {
+  // Add R_m' column variables
+  maxLoopSize += numR;
+  for (j = 0, i = numC; i<= maxLoopSize; ++j ,++i){
+    //*2 as R_n' max and min flow is 2 times greater than max and min of R_n because the outgoing of node R_n is greater by a magnitude of 2 than R_n
+    const double lowerBound = 2*myFloor(rPrimeValues[j]);
+    const double upperBound = 2*myCeil(rPrimeValues[j]);
+    if (lowerBound != upperBound) {
+      glp_set_col_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_col_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+
+  // Add X_nm' column variables numC and numR along in the column.
+  maxLoopSize += numX;
+  for (j = 0, i = numC + numR + 1; i<=maxLoopSize; ++j, ++i){
+    const double lowerBound = myFloor(input[j]);
+    const double upperBound = myCeil(input[j]);
+    if (lowerBound != upperBound) {
+      glp_set_col_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_col_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+
+  // Add X_nm'' column variables numC, numR, and numX along in the column.
+  maxLoopSize += numX;
+  for (j = 0, i = numC + numR + numX + 1; i<=maxLoopSize; ++j, ++i){
+    const double lowerBound = myFloor(input[j]);
+    const double upperBound = myCeil(input[j]);
+    if (lowerBound != upperBound) {
+      glp_set_col_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_col_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+
+  // Add e_t column variables numC, numR, and 2numX along in the column
+  maxLoopSize += numE;
+  for (j = 0, i = numC + numR + 2*numX + 1; i<=maxLoopSize; ++j, ++i){
+    const double lowerBound = myFloor(2*input[j]);
+    const double upperBound = myCeil(2*input[j]);
+    if (lowerBound != upperBound) {
+      glp_set_col_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_col_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+/* ADD COLUMN BOUNDS ENDS HERE *********************************************/
+
+/* ADD ROW COEFFICIENTS STARTS HERE *******************************************************/
+  // Add C_m coefficients and R_n: C_1' + C_2' + .... + C_m' + R_1 + R_2 + .... + R_n'
+  maxLoopSize = numC + numR;
+  for (i=1; i<= maxLoopSize; i++) {
     glp_set_obj_coef(lp, i, 1.0);
   }
 
-  glp_add_rows(lp, size);
+/* ADD ROW COEFFICIENTS ENDS HERE *******************************************************/
 
-  // Set the rows
-  for (int i=1; i<= size; i++){
-    double lowerBound = myFloor(rows[i-1]);
-    double upperBound = myCeil(rows[i-1]);
+/* ADD ROWS BOUNDS STARTS HERE *******************************************************/
+
+  const int numRows = numC + numR + numX + numE;
+  glp_add_rows(lp, numRows);
+
+  // Set bounds for C_m
+  maxLoopSize = numC;
+  for (i = 1; i<=maxLoopSize; ++i){
+    const double lowerBound = 2*myFloor(cPrimeValues[i-1]);
+    const double upperBound = 2*myCeil(cPrimeValues[i-1]);
     if (lowerBound != upperBound) {
       glp_set_row_bnds(lp, i, GLP_DB, lowerBound, upperBound);
     } else {
       glp_set_row_bnds(lp, i, GLP_FX, lowerBound, upperBound);
     }
   }
-/* ADD ROWS AND COLUMNS ENDS HERE *******************************************************/
+
+  // Set bounds for R_n
+  maxLoopSize += numR;
+  for (j=0, i = numC + 1; i<=maxLoopSize; ++i, ++j){
+    const double lowerBound = 2*myFloor(rPrimeValues[j]);
+    const double upperBound = 2*myCeil(rPrimeValues[j]);
+    if (lowerBound != upperBound) {
+      glp_set_row_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_row_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+
+  // Set bounds for X_{n,m}
+  maxLoopSize += numX;
+  for (j=0, i=numC + numR + 1; i<= maxLoopSize; ++i, ++j){
+    const double lowerBound = 2*myFloor(input[j]);
+    const double upperBound = 2*myCeil(input[j]);
+    if (lowerBound != upperBound) {
+      glp_set_row_bnds(lp, i, GLP_DB, lowerBound, upperBound);
+    } else {
+      glp_set_row_bnds(lp, i, GLP_FX, lowerBound, upperBound);
+    }
+  }
+
+  // Set bounds for Z_t = 0
+  maxLoopSize += numE;
+  for (i=numC + numR + numX + 1; i<=maxLoopSize; ++i){
+    glp_set_row_bnds(lp, i, GLP_FX, 0.0, 0.0);
+  }
+
+/* ADD ROWS BOUNDS ENDS HERE *******************************************************/
 
 /* ADD THE DATA FOR EACH ROW STARTS HERE ************************************************/
-  int lastJPos = 1;
-  int newSize = size;
-  for (int i = 1; i < size + 1; ++i){
-    for (int j = lastJPos; j < newSize + 1; ++j){
-      row[j] = input[j];
-      lastJPos = j + 1;
+
+  // Set C_m constraints
+  maxLoopSize = numC;
+  for(int c = 0, i = 1; i<=maxLoopSize; ++i, c+= 3){
+    index[1] = i, row[i] = 1;
+    // maxSize is 1 + size because there will be 1 C_m' and size number of x_nm' variables
+    for (j = 2; j<=1 + size; ++j){
+      const int indexValue = numC + numR + j -1 + c;
+      index[j] = indexValue, row[indexValue] = -1;
     }
-    fillIndexArray(index);
+    glp_set_mat_row(lp, i, 1 + size, index, row);
+  }
+
+  // Set R_n constraints
+  maxLoopSize = numC + numR;
+  for (int ii = 1, i = numC + 1; i<= maxLoopSize; i++, ii++) {
+    index[1] = i, row[i] = 1;
+    for (int c = 0, k = 2, j = 1 + numC; j<= numC + size; ++j, ++k, c+=3){
+      const int indexValue = numC + numR + numX + c + ii;
+      index[k] = indexValue, row[indexValue] = -1;
+    }
+    glp_set_mat_row(lp, i, 1 + size, index, row);
+  }
+
+  // Set X_{n,m} constraints
+  maxLoopSize = numC + numR + numX;
+  for (int i = numC + numR + 1; i <= maxLoopSize; i++){
+    int indexValue = i;
+    index[1] = indexValue, row[indexValue] = 1;
+    indexValue = i + numX;
+    index[2] = indexValue, row[indexValue] = 1;
+    indexValue = i + numX + numX;
+    index[3] = indexValue, row[indexValue] = -1;
     glp_set_mat_row(lp, i, 3, index, row);
-    newSize += lastJPos;
+  }
+
+  // Set Z_t constraints
+  maxLoopSize = numC + numR + numX + numE;
+  for (int i = 1; i<=numE; ++i){
+    int indexValue = i + numC + numR;
+    index[1] = indexValue, row[indexValue] = 1;
+    indexValue = i + numC + numR + numX;
+    index[2] = indexValue, row[indexValue] = -1;
+    glp_set_mat_row(lp, i, 2, index, row);
   }
 
 /* ADD THE DATA FOR EACH ROW ENDS HERE **************************************************/
@@ -137,11 +263,11 @@ int computeSolution(void) {
 
   // Fill in solution
   double perfectValue = glp_get_obj_val(lp);
-  for (int i = 1; i<size + 1; ++i) {
+  for (int i = numC + numR + 1; i<=numC + numR + numX; ++i) {
       solution[i-1] = glp_get_row_prim(lp, i);
   }
 
-  glp_delete_prob(lp); // release memory used for LP
+  // glp_delete_prob(lp); // release memory used for LP
 
   return 1; /* This is not always correct, of course, and needs to be changed. */
 }
